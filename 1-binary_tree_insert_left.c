fff1c8d28d4b5d4e38750b56600f5a65c2d87edc
Elvinooooo <Elvinooooo@github.com>
2024-01-12 02:49:29 -0800

    double free error handled

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,20 +9,20 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	binary_tree_t *temp;
 
-	new_node = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
 		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
+	new_node->parent = parent;
 	if (parent->left != NULL)
 	{
-		temp = parent->left;
-		new_node->left = temp;
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
 	}
 	parent->left = new_node;
 	return (new_node);
